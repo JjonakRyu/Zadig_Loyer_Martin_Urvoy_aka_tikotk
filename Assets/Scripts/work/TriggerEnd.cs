e69d0997d13c515a69b55b2820f14e6566809766
Zaaadig <z_loyer@stu-lisaa.com>
2024-03-30 05:05:12 +0100

    Trigger end script

---
+++ Assets/Scripts/work/TriggerEnd.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnd : MonoBehaviour
+{
+    public GameObject player;
+    public Color defaultColor = Color.white;
+    public ParticleSystem particleSystem1;
+    public ParticleSystem particleSystem2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.GetComponent<CapsuleController>().enabled = false;
+        player.GetComponent<SpriteRenderer>().color = defaultColor;
+        particleSystem1.Stop();
+        particleSystem2.Stop();
+    }
+
+}
