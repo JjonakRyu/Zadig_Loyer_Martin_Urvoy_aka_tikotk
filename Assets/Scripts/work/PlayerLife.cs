3ca73b02a5535dcc2f4aaceb1ac6ce8db764db83
Zaaadig <z_loyer@stu-lisaa.com>
2024-04-04 17:14:20 +0200

    commit menu bg

--- Assets/Scripts/work/PlayerLife.cs
+++ Assets/Scripts/work/PlayerLife.cs
@@ -71,6 +71,8 @@ public class PlayerLife : MonoBehaviour
 
         if (isEnabled && collision.CompareTag("EndAnimation"))
         {
+            isDead = true;
+
             StartCoroutine(EndingAnimation());
         }
     }
